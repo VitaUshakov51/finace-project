806238190efb9759f5561673a41411de24ee592f
VitaUshakov51 <vitavita-98@yandex.ru>
2023-11-16 10:57:11 +0300

    Создание структуры проекта

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,5 @@
 import {Burger} from "./utils/burger.js";
-import {Home} from "./scripts/home.js";
+import {Home} from "../scripts/home.js";
 import {PopUp} from "./utils/pop-up.js";
 PopUp.show();
 PopUp.deleteOperation();
